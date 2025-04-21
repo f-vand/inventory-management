ca0ef1e82cb57978634c130cedb2cd8275d71776
f-vand <f.vand@yahoo.com>
2025-04-21 03:16:05 -0400

    subclass

--- script.js
+++ script.js
@@ -21,3 +21,21 @@
         return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}`
     }
     }
+
+
+
+// Part 2: Subclass
+class PerishableProductProperties extends ProductProperties {
+
+    // @param {String } name - Name of Product
+    // @param {Number} price - Price of product
+    // @param {Number} quantity - remaining stock quantity
+    // @param {String} expirationDate
+
+    constructor(name, price, quantity, expirationDate) {
+    super(name, price, quantity);
+    this.expirationDate = expirationDate;
+    }
+
+
+}
