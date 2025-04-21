59dc5dc7b5a84518820c6f038a995bfc3dbffcee
f-vand <f.vand@yahoo.com>
2025-04-21 02:32:04 -0400

    class and method() cons

---
+++ script.js
@@ -0,0 +1,11 @@
+ // Part 1: Base Class
+ class ProductProperties {
+    // @param {String } name - Name of Product
+    // @param {Number} price - Price of product
+    //@param {Number} quantity - remaining stock quantity
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+      }
+    }
